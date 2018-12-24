f011033065cdc62956d442a07a1c809fde938b5d
Vladimir <vovabokbelg5@gmail.com>
2018-12-24 20:46:30 +0300

    first

--- myprintf.c
+++ myprintf.c
@@ -1,6 +1,6 @@
 #include <myprintf.h>
 
-int		ft_printf1(va_list list, const char *format)
+int		ft_printf1(va_list list, const char *format, specifier_t spec_list[])
 {
 	int result;
 
@@ -19,11 +19,20 @@ int		ft_printf1(va_list list, const char *format)
 
 int		ft_printf(const char *format, ...)
 {
-	va_list	list;
-	int		print;
+	va_list		list;
+	int			print;
+
+	specifier_t	spec_list[] = {
+			{"c", print_char},
+			{"$", print_proc},
+			{"s", print_string},
+			{"d", print_int},
+			{"i", print_int},
+			{NULL, NULL}
+	};
 
 	va_start(list, format);
-	print = ft_printf1(list, format);
+	print = ft_printf1(list, format, spec_list);
 	va_end(format);
 	return (print);
 }
--- myprintf.h
+++ myprintf.h
@@ -15,5 +15,6 @@ typedef struct specifier specifier_t;
 
 
 int		ft_printf(const char *format, ...);
+int		ft_printf1(va_list list, const char *format);
 
 #endif
