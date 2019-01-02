44d9921825802c9af37d9f94cf21ac9c54adda6e
Vladimir <vovabokbelg5@gmail.com>
2019-01-02 21:00:34 +0300

    upd

--- myprintf.c
+++ myprintf.c
@@ -1,11 +1,94 @@
 #include <myprintf.h>
 
-int		ft_printf1(va_list list, const char *format)
+t_specifier	*create_specifier(char *fmt, int *i)
 {
+	t_specifier	*sp;
 
+	if (!(sp = (t_specifier *)malloc(sizeof(t_specifier))))
+		handle_error(0, NULL);
+	sp->flags = get_flags(fmt, i);
+	sp->width = 0;
+	sp->accur = 0;
+	sp->width = get_width(fmt, i);
+	sp->accur = get_accur(fmt, i);
+	sp->size = get_sizemodifier(fmt, i);
+	sp->specifier = (char)get_spec(fmt, i);
+	if (!sp->flags)
+		handle_error(0, &sp);
+	return (convert_big(sp));
 }
 
-int		ft_printf(const char *format, ...)
+int		check_spec(va_list list, int *i, char *fmt)
 {
-	
+	t_specifier	*sp;
+	int			result;
+
+	result = 0;
+	if (fmt[*i] && fmt[*i] == '%')
+	{
+		result += write_int('%');
+		(*i)++;
+		return (result);
+	}
+	if (!(fmt[*i]))
+		return (result);
+	sp = create_specifier(fmt, i);
+	if (!is_valid_spec(&sp, i))
+	{
+		spec_del(&sp);
+		return (result);
+	}
+	result += ft_convert(*sp, list);
+	return (result);
+}
+
+int		ft_printf1(va_list list, char *format)
+{
+	int	i;
+	int	result;
+	int	count;
+
+	i = 0;
+	result = 0;
+	while (format[i])
+	{
+		count = 0;
+		if (format[i] != '%')
+		{
+			while (format[i] && format[i] != '%')
+			{
+				count += write_int(format[i]);
+				i++;
+			}
+		}
+		else
+		{
+			i++;	
+			count += check_spec(list, &i, format);
+		}
+		result += count;
+	}
+	return (result);
+}
+
+int		ft_printf(char *format, ...)
+{
+
+	va_list		list;
+	int			print;
+
+	va_start(list, format);
+	print = ft_printf1(list, format);
+	va_end(list);
+	return (print);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char a = 'a';
+	//char b = 'a';
+    printf("original: lol%-10.2slol%%lol%Clol\n", "checkkkkk", a);
+    ft_printf("test:     lol%-10.2slol%%lol%clol\n", "checkkkkk", a);
+	return 0;
 }
--- valid.c
+++ valid.c
@@ -1,26 +1,26 @@
 #include <myprintf.h>
 
-int is_flag(const char c)
+int is_flag(char c)
 {
 	return (ft_strchr("-+ #0", c) != NULL);
 }
 
-int is_width(const char c)
+int is_width(char c)
 {
-	return (ft_strchr(".$*", c) != NULL);
+	return (ft_strchr("$*.", c) != NULL);
 }
 
-int is_type(const char c)
+int is_type(char c)
 {
 	return (ft_strchr("dDioOuUxXeEfFgGaAcCsSpn%", c) != NULL);
 }
 
-int is_size(const char c)
+int is_size(char c)
 {
-	return (ft_strchr("lh", c) != NULL);
+	return (ft_strchr("lhL", c) != NULL);
 }
 
-int is_ok(const char c)
+int is_ok(char c)
 {
 	return (is_size(c) || is_type(c) || is_width(c) || is_flag(c));
 }
